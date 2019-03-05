041d4865fd879bfdb60095b48bdf7b3ebad3a728
Ainurrofika <48191479+Ainurrofika@users.noreply.github.com>
2019-03-05 00:25:57 -1200

    Tugas ke 1

---
+++ Ainurrofika17090002_4D.java
@@ -0,0 +1,15 @@
+
+package halo;
+
+/**
+ *
+ * @author ainur rofika
+ */
+public class Ainurrofika17090002_4D {
+    public static void main (String[] args){
+        System.out.println("Halo nama saya Ainur rofika");
+        System.out.println("Prodi informatika");
+        System.out.println("Kelas 4D");
+        System.out.println("NIM 17090002");
+    }
+}
